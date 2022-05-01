92f7c45986ea2f7b381007cb4919034e7ad2d9ce
Bing Rong Jiang <bingjiang0212@gmail.com>
2022-05-01 15:31:16 +1000

    flask_route practice

--- questions/flask_practice/flask_practice.py
+++ questions/flask_practice/flask_practice.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from distutils.log import debug
+import mimetypes
+from flask import Flask, send_file, redirect
 APP = Flask(__name__)
 
 
@@ -7,5 +9,15 @@ def home():
     return "Hello World!"
 
 
+@APP.route("/rickroll")
+def rick_roll():
+    return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley")
+
+
+@APP.route("/cat")
+def get_cat():
+    return send_file('./cat.jpg', mimetype='image/jpg')
+
+
 if __name__ == "__main__":
-    APP.run(port=10000)
+    APP.run(port=10000, debug=True)
